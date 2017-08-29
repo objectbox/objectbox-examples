c2493025ea2ce88a5a96f81adaf882fe0cf3d06f
greenrobot Team <opensource+team@greenrobot>
2017-08-29 15:53:51 +0200

    Add ToOne and ToMany example entities and demo code.

---
+++ objectbox-example/src/main/java/io/objectbox/example/Customer.java
@@ -0,0 +1,17 @@
+package io.objectbox.example;
+
+import io.objectbox.annotation.Backlink;
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+import io.objectbox.relation.ToMany;
+
+@Entity
+public class Customer {
+
+    @Id
+    public long id;
+
+    @Backlink
+    public ToMany<Order> orders;
+
+}
