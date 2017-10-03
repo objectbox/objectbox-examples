d1e2ad1121dc1a664056d5e67c01e5a7a5800271
Markus <markus@greenrobot>
2017-10-03 10:20:45 +0100

    use ObjectBox browser in objectbox-example

--- objectbox-example/src/main/java/io/objectbox/example/App.java
+++ objectbox-example/src/main/java/io/objectbox/example/App.java
@@ -1,11 +1,9 @@
 package io.objectbox.example;
 
 import android.app.Application;
-import android.os.Environment;
-
-import java.io.File;
 
 import io.objectbox.BoxStore;
+import io.objectbox.android.AndroidObjectBrowser;
 
 public class App extends Application {
 
@@ -17,16 +15,10 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-//        if (EXTERNAL_DIR) {
-//            // Example how you could use a custom dir in "external storage"
-//            // (Android 6+ note: give the app storage permission in app info settings)
-//            File directory = new File(Environment.getExternalStorageDirectory(), "objectbox-notes");
-//            boxStore = MyObjectBox.builder().androidContext(App.this).directory(directory).build();
-//        } else {
-        // This is the minimal setup required on Android
         boxStore = MyObjectBox.builder().androidContext(App.this).build();
-//        }
+        if (BuildConfig.DEBUG) {
+            new AndroidObjectBrowser(boxStore).start(this);
+        }
     }
 
     public BoxStore getBoxStore() {
