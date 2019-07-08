64908e17086f411a9cbf20f9e236260b594ef673
greenrobot Team <opensource+team@greenrobot>
2019-07-08 12:45:46 +0200

    Android: add Play Core to detect incomplete installs, link to docs.

--- android-app-kotlin/src/main/java/io/objectbox/example/kotlin/App.kt
+++ android-app-kotlin/src/main/java/io/objectbox/example/kotlin/App.kt
@@ -1,6 +1,7 @@
 package io.objectbox.example.kotlin
 
 import android.app.Application
+import com.google.android.play.core.missingsplits.MissingSplitsManagerFactory
 
 class App : Application() {
 
@@ -9,6 +10,13 @@ class App : Application() {
     }
 
     override fun onCreate() {
+        // Optional: if you distribute your app as App Bundle, provides detection of incomplete
+        // installs due to sideloading and helps users reinstall the app from Google Play.
+        // https://docs.objectbox.io/android/app-bundle-and-split-apk
+        if (MissingSplitsManagerFactory.create(this).disableAppIfMissingRequiredSplits()) {
+            return // Skip app initialization.
+        }
+
         super.onCreate()
         ObjectBox.init(this)
     }
