656eed15b49518ad88b36301ba0271c88423b771
greenrobot Team <opensource+team@greenrobot>
2020-08-04 08:47:37 +0200

    Init android-app-multiprocess example.

---
+++ android-app-multiprocess/src/main/java/com/example/android_app_multiprocess/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_app_multiprocess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
