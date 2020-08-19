5a120a6b74cc4e6c5c5c17695c56680060ddcef8
Markus <markus@greenrobot>
2020-08-19 17:29:05 +0200

    Update to ObjectBox 2.7.1 and demonstrate handling issues caused by devices with a broken file system

--- android-app/src/main/java/io/objectbox/example/ObjectBox.java
+++ android-app/src/main/java/io/objectbox/example/ObjectBox.java
@@ -4,16 +4,25 @@ import android.content.Context;
 import android.util.Log;
 
 import io.objectbox.BoxStore;
+import io.objectbox.BoxStoreBuilder;
 import io.objectbox.android.AndroidObjectBrowser;
+import io.objectbox.exception.FileCorruptException;
 
 public class ObjectBox {
 
     private static BoxStore boxStore;
 
     static void init(Context context) {
-        boxStore = MyObjectBox.builder()
-                .androidContext(context.getApplicationContext())
-                .build();
+        BoxStoreBuilder storeBuilder = MyObjectBox.builder()
+                .androidContext(context.getApplicationContext());
+        try {
+            boxStore = storeBuilder.build();
+        } catch (FileCorruptException e) { // Demonstrate handling issues caused by devices with a broken file system
+            Log.w(App.TAG, "File corrupt, trying previous data snapshot...", e);
+            // Retrying requires ObjectBox 2.7.1+
+            storeBuilder.usePreviousCommit();
+            boxStore = storeBuilder.build();
+        }
 
         if (BuildConfig.DEBUG) {
             Log.d(App.TAG, String.format("Using ObjectBox %s (%s)",
