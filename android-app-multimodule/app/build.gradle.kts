efca75d7d55c5a19ae541a8740ebbc978daca075
greenrobot Team <opensource+team@greenrobot>
2022-05-17 11:05:07 +0200

    Module example: add basic unit test.

--- android-app-multimodule/app/build.gradle.kts
+++ android-app-multimodule/app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 
 val _compileSdkVersion: Int by rootProject.extra
 val _targetSdkVersion: Int by rootProject.extra
+val objectboxVersion: String by rootProject.extra
 
 android {
     compileSdk = _compileSdkVersion
@@ -37,6 +38,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    testOptions {
+        unitTests {
+            // For Robolectric tests
+            isIncludeAndroidResources = true
+        }
+    }
 }
 
 dependencies {
@@ -50,7 +57,17 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    testImplementation("androidx.test:core:1.4.0")
+    testImplementation("org.robolectric:robolectric:4.8.1")
+    // Add the ObjectBox native library to run unit tests on this development machine.
+    // A library for Linux (x86_64), Windows (x86 and x86_64) and macOS (x86 and M1) is available.
+    testImplementation("io.objectbox:objectbox-linux:$objectboxVersion")
+    testImplementation("io.objectbox:objectbox-macos:$objectboxVersion")
+    testImplementation("io.objectbox:objectbox-windows:$objectboxVersion")
+    // There are also ARM versions available for Linux,
+    // but these are typically not architectures used on development machines.
+    // testImplementation("io.objectbox:objectbox-linux-arm64:$objectboxVersion")
+    // testImplementation("io.objectbox:objectbox-linux-armv7:$objectboxVersion")
 }
