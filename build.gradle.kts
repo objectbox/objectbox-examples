6b9c3eb6b6208d3cd9eafea0f614c0f38e90d6b6
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2023-08-23 11:16:29 +0200

    Update Gradle to 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ tasks.wrapper {
 
 // Reject preview releases for dependencyUpdates task
 fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
