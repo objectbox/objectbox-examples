08e209e647d371ec24ae80992d6d77b81eb0208b
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2024-06-03 15:58:56 +0200

    Update ObjectBox to 4.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 buildscript {
-    val objectboxVersion by extra("4.0.0")
+    val objectboxVersion by extra("4.0.1")
 
     // For Android projects
     val _compileSdkVersion by extra(33) /* Android 13 (TIRAMISU) */
