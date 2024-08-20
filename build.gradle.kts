d61f91fc8ff19e965341eb08f6dbcddb6bd0b379
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2024-08-20 13:38:58 +0200

    Update ObjectBox to 4.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 buildscript {
-    val objectboxVersion by extra("4.0.1")
+    val objectboxVersion by extra("4.0.2")
 
     // For Android projects
     val _compileSdkVersion by extra(34) /* Android 14 (UPSIDE_DOWN_CAKE) */
