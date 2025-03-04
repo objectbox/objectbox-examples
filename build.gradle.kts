00852fc98431dbf509ca9d016d40393314a6ebc9
Uwe <noreply+uwe@objectbox.io>
2025-03-04 14:40:41 +0100

    Update ObjectBox to 4.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 buildscript {
-    val objectboxVersion by extra("4.1.0")
+    val objectboxVersion by extra("4.2.0")
 
     // For Android projects
     val _compileSdkVersion by extra(34) /* Android 14 (UPSIDE_DOWN_CAKE) */
